e5e008272855bd32e113f66a995ad3810a93b974
Fredrick Kristoffersson <08_krogen.delar@icloud.com>
2024-11-27 12:04:52 +0100

    Initial commit (starting the project)

---
+++ src/main/java/org/example/lab2springboot/Lab2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.lab2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2SpringBootApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(Lab2SpringBootApplication.class, args);
+        }
+
+}
